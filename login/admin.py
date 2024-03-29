509f58e2c5554296b9e4d738753636d063bab193
floodro <floodro11@gmail.com>
2024-03-30 03:39:36 +0800

    Added database models for account types.

--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Director, Administrator, Performer
 
 # Register your models here.
+admin.site.register(Director)
+admin.site.register(Administrator)
+admin.site.register(Performer)
--- login/models.py
+++ login/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Director(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    campus = models.CharField(max_length=200)
+    username = models.EmailField(max_length=200)
+    password = models.CharField(max_length=200)
+    fullAccess = models.BooleanField(default=True)
+
+    def __str__(self):
+        return  f"{self.first_name} {self.last_name}"
+
+class Administrator(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    campus = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return  f"{self.first_name} {self.last_name}"
+
+class Performer(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    sr_code = models.CharField(max_length=200)
+    campus = models.CharField(max_length=200)
+    username = models.EmailField(max_length=200)
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.sr_code
